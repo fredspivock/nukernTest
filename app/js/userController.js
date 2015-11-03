0140d722d4a29df4f52b0d29efb693a337c9ad52
fredspivock <fredspivock@gmail.com>
2015-11-02 20:51:58 -0500

    fixed get, put, post

--- app/js/userController.js
+++ app/js/userController.js
@@ -8,17 +8,16 @@ angular.module("nukernUserController", ['ui.bootstrap'])
 		//get single user info
 		$http({
 			method: 'GET',
-			//http://nukern-test.herokuapp.com/api/clients
-			url: 'clients.json'
+			url: 'http://nukern-test.herokuapp.com/api/clients'
         })
 		.then(function(data){
 			$scope.users = data.data;
 		}, 
 		function(){
-			alert("could not connect")
+			alert("could not connect");
 		});
 
-		//create Modal based on id
+		//create Modal to edit single user
 		$scope.userModalClick = function(id) {
 			$scope.id = id;
 			var modalInstance = $uibModal.open({
@@ -34,9 +33,22 @@ angular.module("nukernUserController", ['ui.bootstrap'])
 				}
 			});
 		}
+
+		//create Modal to add single user
+		$scope.newUserModalClick = function() {
+
+			var modalInstance = $uibModal.open({
+
+				templateUrl:"partials/newUserModal.html",
+				controller: "AddModalUserCtrl",
+				size: "lg"
+			});
+
+		}
+		
 	}]);
 
-/* Controller for single user modal */
+/* Controller to edit single user modal */
 angular.module("nukernUserController").controller("ModalUserCtrl", [ '$http', '$scope', '$uibModalInstance', 'id',
 	function($http, $scope, $uibModalInstance, id){
 
@@ -58,28 +70,83 @@ angular.module("nukernUserController").controller("ModalUserCtrl", [ '$http', '$
 			}
 		);
 
-		//returns edited user data from modal form and posts it
 		$scope.ok = function() {
-			uibModalInstance.close($scope.user);
-			$scope.user = uibModalInstance.result;
+
+			$uibModalInstance.close();
 
 			//Posts data
 			$http({
-				method: "POST",
-				url: "clients" + $scope.id + '.json',
+				method: "PUT",
+				url: "http://nukern-test.herokuapp.com/api/clients/" + $scope.id,
 				data: $scope.user
 			})
 			.then(
 				function(){
-
 				},
 				function(){
-
 				}
 			);
 
 		};
 
+		//function if user presses cancel on modal
+		$scope.cancel = function() {
+			$uibModalInstance.dismiss('cancel');
+		}
+
+
+	}]);
+
+
+/* Add User Controller */
+
+angular.module("nukernUserController")
+.controller("NewUserCtrl" ,['$http', '$scope', '$uibModal',
+
+	function($http, $scope, $uibModal){
+
+		//create Modal to add single user
+		$scope.newUserModalClick = function() {
+
+			var modalInstance = $uibModal.open({
+
+				templateUrl:"partials/newUserModal.html",
+				controller: "AddModalUserCtrl",
+				size: "lg"
+			});
+
+		}
+		
+	}]);
+
+
+/*Controller to add a  user modal */
+
+angular.module("nukernUserController").controller("AddModalUserCtrl", [ '$http', '$scope', '$uibModalInstance',
+	function($http, $scope, $uibModalInstance){
+
+		$scope.user;
+
+		$scope.ok = function() {
+
+			$uibModalInstance.close();
+
+			//Posts data
+			$http({
+				method: "POST",
+				url: "http://nukern-test.herokuapp.com/api/clients/",
+				data: $scope.user
+			})
+			.then(
+				function(){
+				},
+				function(){
+				}
+			);
+
+		};
+
+		//function if user presses cancel on modal
 		$scope.cancel = function() {
 			$uibModalInstance.dismiss('cancel');
 		}
